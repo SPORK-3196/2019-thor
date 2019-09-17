07649fdbaa03c4186736cf700e2c11eee33f1f02
Robbie Mones <1170951@pinelakeprep.org>
2019-09-17 19:15:59 -0400

    Update IntakeWithTriggers.java

--- src/main/java/frc/robot/commands/IntakeWithTriggers.java
+++ src/main/java/frc/robot/commands/IntakeWithTriggers.java
@@ -1,5 +1,24 @@
 /*----------------------------------------------------------------------------*/
 /* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class IntakeWithTriggers extends Command {
+  public IntakeWithTriggers() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(Robot.intake);
+  }
+
+  // Called just before this Command runs the first time
   @Override
   protected void initialize() {
   }

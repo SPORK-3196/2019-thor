388c981a4bf6c568b7641213183822f58b6612c2
Robbie Mones <1170951@pinelakeprep.org>
2019-09-17 18:53:31 -0400

    changed name of controller

--- src/main/java/frc/robot/commands/DriveWithJoystick.java
+++ src/main/java/frc/robot/commands/DriveWithJoystick.java
@@ -8,11 +8,15 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+
 
 public class DriveWithJoystick extends Command {
   public DriveWithJoystick() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
+    requires(Robot.drivetrain);
   }
 
   // Called just before this Command runs the first time
@@ -23,6 +27,10 @@ public class DriveWithJoystick extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    double xInput = Robot.controllerPrimary.getX(Hand.kLeft);
+    double yInput = Robot.controllerPrimary.getY(Hand.kRight);
+
+    Robot.drivetrain.drivetrain.arcadeDrive(yInput, xInput);
   }
 
   // Make this return true when this Command no longer needs to run execute()
